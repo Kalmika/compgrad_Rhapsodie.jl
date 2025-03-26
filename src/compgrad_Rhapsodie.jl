1f214421359532322a36feaf7e5bb614f095025a
André Ferrari <andferrari@gmail.com>
2025-03-26 17:43:24 +0100

    true star

--- src/compgrad_Rhapsodie.jl
+++ src/compgrad_Rhapsodie.jl
@@ -7,7 +7,94 @@ using InterpolationKernels
 using HDF5
 using LinearAlgebra
 
-export comp_grad, init_rhapsodie, generate_star
+export comp_grad, init_rhapsodie, generate_star, init_rhapsodie2
+
+function init_rhapsodie2(;alpha = 1e-2, write_files=false, path = "default")
+    
+    (path ==  "default") && (path = replace(pathof(compgrad_Rhapsodie), "src/compgrad_Rhapsodie.jl" => "data"))
+    path_disk = path*"/sample_for_rhapsodie_128x128.h5"
+    path_star = path*"/star_cropped.fits"
+    ker = CatmullRomSpline(Float64, Flat)
+
+    # load disk
+    
+    object_params=ObjectParameters((128,128),(64.,64.))  # check with file!
+
+    dset = h5open(path_disk, "r")
+    I = read(dset["disk_data"])[:,:,1]
+    Ip = read(dset["disk_data"])[:,:,2]
+    θ = read(dset["disk_theta"])
+    close(dset)
+
+    STAR = readfits(path_star)
+    
+    coef = alpha*maximum(STAR)/maximum(I)
+
+    S = PolarimetricMap("intensities", coef*(I - Ip) + STAR, coef*Ip, θ) 
+
+    # create model
+    
+    data_params=DatasetParameters((128,256), 4, 1, 1, (64.,64.))
+    indices=get_indices_table(data_params)
+    polar_params=set_default_polarisation_coefficients(indices)
+    ker=CatmullRomSpline(Float64, Flat)
+    field_params=FieldTransformParameters[]
+
+    for i=1:data_params.frames_total
+        push!(field_params, FieldTransformParameters(ker,
+                                                    0.,
+                                                    (0.,0.),
+                                                    (-10.7365 , 1.39344),
+                                                    polar_params[i][1],
+                                                    polar_params[i][2]))
+    end
+
+    field_transforms=load_field_transforms(object_params,
+                                           data_params,
+                                           field_params)
+   
+    psf_center=readdlm(path*"/PSF_centers_Airy.txt");
+    psf=readfits(path*"/PSF_parametered_Airy.fits");
+    blur=set_fft_operator(object_params,(psf[1:end÷2,:]'), psf_center[1:2])[1];
+
+    H = DirectModel(size(S), (128,256,4),S.parameter_type,field_transforms,blur)
+    BadPixMap=Float64.(rand(0.0:1e-16:1.0,data_params.size).< 0.9);
+    # BadPixMap=ones(data_params.size)
+
+    # compute measurements
+
+    data, weight = data_simulator(BadPixMap, field_transforms, blur, S);
+    S_convolved = PolarimetricMap("stokes", cat(blur*S.I, blur*S.Q, blur*S.U, dims=3)) 
+
+    if write_files == true
+        if prod(readdir() .!= "test_results")     
+            mkdir("test_results")
+        end
+
+        writefits("test_results/DATA_$(data_params.size[1]).fits",
+            ["TYPE" => "data"],
+            mapslices(transpose,data,dims=[1,2]), overwrite=true)
+
+        writefits("test_results/WEIGHT_$(data_params.size[1]).fits", 
+            ["TYPE" => "weights"],
+            mapslices(transpose,weight,dims=[1,2]), overwrite=true)
+
+        write(S, "test_results/TRUE_$(data_params.size[1]).fits")
+
+        write(S_convolved, "test_results/TRUE_convolved_$(data_params.size[1]).fits")
+    end
+    
+    # DataSet for gradient computation
+    D = Dataset(data, weight , H)
+
+    Ip = zeros(object_params.size);
+    θ = zeros(object_params.size);
+    STAR_p = PolarimetricMap("intensities", STAR, Ip, θ)
+
+    nAS = sum(D.weights .* abs2.(D.direct_model*STAR_p))
+
+    return D, STAR_p.I, nAS
+end
 
 
 function init_rhapsodie(;alpha = 1.0, write_files=false, path_disk = "default")
